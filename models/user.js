4a830c536c46890c081158eb911b35e1af2d86c3
Song <quanpengsong@gmail.com>
2019-05-15 13:05:33 +0800

    fix login bug

--- models/user.js
+++ models/user.js
@@ -3,21 +3,31 @@ const db = new sqlite3.Database('data.db');
 
 class User {
     static checkCredential(id, password, userType, callback) {
-        db.get('SELECT password FROM '+ userType + ' WHERE '+userType+'ID'+' = '+id, (error, data)=>{
+        function checkPassword(error, data) {
             let valid = false;
-            if(error){  // An error occurred while querying the database
+            if (error) {  // An error occurred while querying the database
                 console.error(error.message);
-            }else {
-                if(data===undefined){  // No eligible query items
+            } else {
+                if (data === undefined) {  // No eligible query items
                     valid = false;
-                }else {
-                    if(password===data.password){
+                } else {
+                    if (password === data.password) {
                         valid = true;
                     }
                 }
             }
             callback(error, valid);
-        })
+        }
+        if(userType==="student"){
+            db.get('SELECT password FROM student WHERE studentID = ?',id, checkPassword);
+        }else if(userType==="admin"){
+            db.get('SELECT password FROM admin WHERE adminID = ?',id, checkPassword);
+        }else if(userType==="teacher"){
+            db.get('SELECT password FROM teacher WHERE teacherID = ?',id, checkPassword);
+        }else {
+            console.error("Unexpected user type: "+userType);
+            callback(undefined, false);
+        }
     };
 
     static updatePassword(id, userType, newPassword, callback) {
